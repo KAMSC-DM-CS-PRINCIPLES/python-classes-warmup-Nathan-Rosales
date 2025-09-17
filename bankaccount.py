3c63f9a3e4a0170216f6fcfff399e545533c6218
Nathan-Rosales <nathan08.rosales@gmail.com>
2025-09-17 13:38:26 -0400

    Update bankaccount.py

--- bankaccount.py
+++ bankaccount.py
@@ -2,4 +2,27 @@
 
 if __name__ == "__main__":
     # create BankAccount below this
+
+    class BankAccount:
+        def _init_(intial):
+            if initial == null:
+                raise ValueError
+            else:
+                balance = initial
+
+    def deposit(dep):
+        if dep > 0:
+            balance += dep
+        return balance
+    
+    def withdraw(draw):
+        if draw > 0:
+            balance -= draw
+            return balance
+        else:
+            return "Insufficient Funds"
+        
+    def get_balance():
+        return balance
+
     pass
