8bd8b370aea005fb00111b680140ca4e751dedf9
Nathan-Rosales <nathan08.rosales@gmail.com>
2025-09-22 07:46:46 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,5 +1,20 @@
 # TODO create class Student
 
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
+    def get_name(self):
+        return self.name
+    
+    def get_grade(self):
+        return self.grade
+    
+    def set_grade(self, newGrade):
+        self.grade = newGrade
+
+
 if __name__ == "__main__":
     # create Student below this
     pass
