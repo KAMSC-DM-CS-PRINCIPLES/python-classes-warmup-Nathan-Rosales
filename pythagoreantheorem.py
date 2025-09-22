c85f2164999ff0c947e41e0ad3f7aa8a80e46de9
Nathan-Rosales <nathan08.rosales@gmail.com>
2025-09-22 07:37:11 -0400

    Update pythagoreantheorem.py

--- pythagoreantheorem.py
+++ pythagoreantheorem.py
@@ -1,5 +1,13 @@
 # TODO create class PythagoreanTheorem
 
+class PythagoreanTheorem:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def hypotenuse(self):
+        return math.sqrt(a**2 + b**2)
+
 if __name__ == "__main__":
     # create PythagoreanTheorem below this
     pass
